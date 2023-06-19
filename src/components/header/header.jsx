73544abb269a3611107eac5800358de0a86d22db
Mateuszmeteo <mateusz.trepkowski@gmail.com>
2023-06-19 03:27:41 +0200

    no-function-menu

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,12 +1,48 @@
 
-import React from "react"
+import React, {useState, useEffect} from "react"
 import styles from './header.module.scss'
 
 
 
 export const Header = () => {
 
+    const [isMenuOpen, setMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setMenuOpen(!isMenuOpen)
+    }
+
+    const handleMenuOutsideClick = (event) => {
+        if (!event.target.closest('.menu')) {
+          setMenuOpen(false);
+        }
+      };
+    
+      useEffect(() => {
+        document.addEventListener('click', handleMenuOutsideClick);
+    
+        return () => {
+          document.removeEventListener('click', handleMenuOutsideClick);
+        };
+      }, []);
+
     return (
-        <div className={styles.head__div}></div>
+        <div className={styles.head__div}>
+            <div>logo</div>
+            <div>
+                <button>0000</button>
+                <button>9999</button>
+            </div>
+            <div>
+                <button onClick={toggleMenu} >menu</button>
+                {isMenuOpen && (
+                    <ul className={styles.head__menuList}>
+                        <li><p>opoopo</p></li>
+                        <li><p>opoopo</p></li>
+                        <li><p>opoopo</p></li>
+                    </ul>
+                )}
+            </div>
+        </div>
     )
 }
